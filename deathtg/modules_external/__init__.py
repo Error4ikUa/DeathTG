3ea81cc8405097687412af07e9f8d0fb4966dfef
Error4ik <78443482+Error4ikUa@users.noreply.github.com>
2026-05-15 01:13:18 +0300

    Add external modules package

---
+++ deathtg/modules_external/__init__.py
@@ -0,0 +1,5 @@
+"""External DeathTG user modules package.
+
+Downloaded modules are loaded under this package so relative imports like
+`from ..command import command` can resolve against the main `deathtg` package.
+"""
