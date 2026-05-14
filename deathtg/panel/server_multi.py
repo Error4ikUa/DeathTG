43f2f02d60cd863a9d3709ab84b0e7110e6888e6
Error4ik <78443482+Error4ikUa@users.noreply.github.com>
2026-05-14 19:45:18 +0300

    Connect reconnect routes to panel

--- deathtg/panel/server_multi.py
+++ deathtg/panel/server_multi.py
@@ -1,4 +1,6 @@
 from deathtg.panel.server_v2 import app
 from deathtg.panel.pages import router as pages_router
+from deathtg.panel.re_auth import router as reconnect_router
 
 app.include_router(pages_router)
+app.include_router(reconnect_router)
